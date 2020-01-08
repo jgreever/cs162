df1aee70a2e2f1e31d9d14ccf935bd016a9fe139
Justin Greever <jgreever@gmail.com>
2020-01-08 09:06:55 -0800

    Added Lab1 example program

---
+++ cs162_lab1.cpp
@@ -0,0 +1,13 @@
+//My name: Justin Greever
+//Sample program to get familiar with linux
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Output a message to welcome the user!
+    cout << "Welcome to Computer Science!";
+
+    return 0;
+}

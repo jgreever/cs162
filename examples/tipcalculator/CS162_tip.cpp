afe7cac7c7e6fc95ed07805b0371bba734b72dbf
Justin Greever <jgreever@gmail.com>
2020-01-16 13:22:28 -0800

    made changes to CS162_tip calculator app

--- examples/tipcalculator/CS162_tip.cpp
+++ examples/tipcalculator/CS162_tip.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 // Justin Greever
 // This program will help a user determine the amount of 
 // a tip to leave based on total and quality of service
+//
+// toupper - return the uppercase value of the character passed in
+// tolower - return the lowercase value of the character passed in
+// isupper - check if the character passed in is upper and return T/F
+// islower - check if the character passed in is lower and return T/F
+//
 
 int main()
 {
@@ -19,17 +26,19 @@ int main()
 	cout << endl << endl;
 
 	//Get the total amount of the bill
-	cout << "Please enter the total amount of the bill $";
-	cin >> total;
-	cout << "We recieved a total bill of $" << total << endl;
-	cout << "Is this correct? 'Y' for Yes, 'N' for No: ";
-	cin >> response;
-
-	if (response != 'Y' &&  response != 'y')
+	do
 	{
-		cout << "Please enter the value of the bill: $";
+		cout << "Please enter the total amount of the bill $";
 		cin >> total;
-	}
+		cout << "We recieved a total bill of $" << total << endl;
+		cout << "Is this correct? 'Y' for Yes, 'N' for No: ";
+		cin >> response;
 
+		if (response != 'Y' &&  response != 'y')
+		{
+			cout << "Ok! We will try again..." <<endl;
+		}
+	//} while ((response !='Y' && response !='y') || total < 0.0);
+        } while (toupper(response) != 'Y' || total < 0.0);
 	return 0;
 }

08bcddc0c2eac91956b556dbd4fcae119aad2d49
Justin Greever <jgreever@gmail.com>
2020-01-21 13:45:31 -0800

    Added two functions in the hangman game

--- examples/hangman/hangman.cpp
+++ examples/hangman/hangman.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <cctype>
+#include<cstring>
+using namespace std;
+
 /* 
  * Justin Greever
  * CS162
@@ -7,10 +12,55 @@
  * all code in the 'int main()' section.
  */
 
-#include <iostream>
-using namespace std;
+/* Constants - globally so that all functions have access 
+ * use UPPERCASE so you can find constants easier
+ */
+const int SIZE = 21; /* MAX 20 character word + 1 character for '\0' */
+
+/* Prototype - telling the compiler that new functions exist */
+void rules(); /* Explins the rules of the program */
+int get_secret(char secret_word[]);
 
 int main()
 {
+  char secret[SIZE]; /* Holds players secret word to be guessed */
+  char answer[SIZE]; /* Start as underscores */
+  int len = 0; /* length of the secret word */
 
+  rules(); /* Explain the rules in this function call */
+  len = get_secret(secret); /* get the secret word from the user */
+
+  cout << "Testing the length is: " << len << endl
+       << "And the secret word is: " << secret << endl << endl;
+
+  cout << "Thank you for paying the hangman game!!! \n\n";
+  return 0;
+}
+
+/* code function after main */
+/* This function will explain the rules on how to
+ * use the hangman program to the user 
+ */
+void rules()
+{
+  cout << "Welcome to the Hangman Game! \n\n"
+       << "This is a two player game. One player selcts"
+       << "a secret word\nand the other player guesses.";
+  cout << "Let's have fun!!!" << endl << endl;
+}
+
+/* prompt and read in the secret word */
+int get_secret(char secret[])
+{
+  cout << "Player 1, please enter the secret word: ";
+  cin.width(SIZE);
+  cin >> secret;
+  cin.ignore(100, '\n');
+  
+  /* clear the screen */
+  for (int i = 0; i < 100; ++i)
+    cout << '\n';
+
+  /* return the length of the secret word */
+  return strlen(secret);
 }

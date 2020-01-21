aac84dd4fcbd056bb7dd9ffade0fe185ce814a0a
Justin Greever <jgreever@gmail.com>
2020-01-21 13:23:21 -0800

    changed names to reflect actual program use

---
+++ examples/hangman/hangman.cpp
@@ -0,0 +1,16 @@
+/* 
+ * Justin Greever
+ * CS162
+ * Hangman - This is a hangman program
+ * that will teach me how to use functions
+ * and create my own instead of putting
+ * all code in the 'int main()' section.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}

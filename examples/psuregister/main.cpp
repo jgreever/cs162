cbcebe9c83b8bcdf9868a02f0919ebec14f50b81
Justin Greever <jgreever@gmail.com>
2020-01-31 19:04:44 -0800

    worked on psuregister code a bit more

--- examples/psuregister/main.cpp
+++ examples/psuregister/main.cpp
@@ -11,10 +11,16 @@
 #include <iostream>
 using namespace std;
 
+const int SIZE = 21;
+
 void read_name(char first[], char last[]);
 
 int main()
 {
+  char first[SIZE];
+  char last[SIZE];
+
+  read_name(first, last);
 
   return 0;
 }
@@ -23,7 +29,11 @@ void read_name(char first[], char last[])
 {
   cout << "Please enter your first name: ";
   cin.get(first,41,'\n');
+  cin.ignore(100,'\n');
+  cout << first << endl;
   cout << "Please enter your last name: ";
   cin.get(last,41,'\n');
-cout << "You entered: " << first << last << endl;
+  cin.ignore(100,'\n');
+  cout << last << endl;
+  cout << "You entered: " << first << " " << last << endl;
 }

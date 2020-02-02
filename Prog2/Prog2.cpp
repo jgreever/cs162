cd7fb16c607a2103185d9e5880287297c4bf8ef8
Justin Greever <jgreever@gmail.com>
2020-02-01 17:04:56 -0800

    added function to read user input for the notepad app and ensured it read input and echoed it back as well as letting main() access the data

--- Prog2/Prog2.cpp
+++ Prog2/Prog2.cpp
@@ -21,18 +21,34 @@
 #include <cstring>
 using namespace std;
 
-/* Global constants here */
+/* Globals here (CONST only!) */
+const int SSIZE = 131; /* Sentence size MAX 130 */
+const int PSIZE = 301; /* Paragraph size MAX 300 */
 
 /* Prototypes Here */
+void read_input(char userInput[]); /* prototype for reading input from the user */
 
 /* Start main() */
 int main()
 {
+  char userInput[PSIZE];
+
   /* Create any variables needed */
+  char userInput[SSIZE];
+
+  /* the main code goes here */
+  read_input(userInput);
 
   /* Exit the program gracefully */
   return 0;
 }
 
 /* Create functions here */
-
+void read_input(char userInput[]) /* this function will grab input to store for later */
+{
+  cout << "Enter your text below. Press enter to start a new paragraph, " << endl;
+  cout << "and put a # on a blank line and press enter to quit." << endl;
+  cin.get(userInput,130,'\n');
+  cin.ignore(100,'\n');
+  cout << userInput << endl;
+}

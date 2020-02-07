f536090e7e08633849c76b7cc0acbcb918d44b12
Justin Greever <jgreever@gmail.com>
2020-02-06 20:45:46 -0800

    Added base code to Prog3.cpp file

---
+++ Prog3/Prog3.cpp
@@ -0,0 +1,31 @@
+/* Justin Greever
+ * CS162  * Program 3
+ * 2/6/2020
+ *
+ * This program will help keep track of my favorite activities.
+ * It will keep track of the following:
+ * a. Name of the activity
+ * b. Where it is located
+ * c. Time of year
+ * d. Description (130 chars max)
+ * e. Thoughts on why its my favorite and why I recoomend it (200 chars max)
+ * f. Supplies requires/attire
+ * g. Specific date/date range
+ * This will be done using functions and structures. Arrays will be a huge
+ * part of this program as well. Here we go!
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <cstring>
+using namespace std;
+
+/* Constants */
+
+/* Prototypes */
+
+/* main() */
+
+/* Functions */
+

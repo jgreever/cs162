674e7404f3eaca476f65bd8fc0ed350ea3dd170b
Justin Greever <jgreever@gmail.com>
2020-02-25 12:23:53 -0800

    Fixed header comments on files

--- Prog4/function.cpp
+++ Prog4/function.cpp
@@ -2,28 +2,35 @@
 //2/22/2020
 //CS162
 //Program 4
-//
+//This program will help keep track of user
+//activites. It 'can' be used to store to a 
+//file if needed, but the main points are to
+//use structs/classes/nodes to create the same
+//program as our last one, storing user entered
+//activites with the possibility of recalling 
+//from a file later.
+
 #include "header.h"
 #include "class.h"
 
-activity::activity()
+activity::activity()      //Make that constructor work for us!
 {
-    head = NULL;
-    tail = NULL;
-    current = NULL;
-    char act = '\0';
-    char loc = '\0';
-    char timeoy = '\0';
-    char spectime = '\0';
-    char thoughts = '\0';
-    char desc = '\0';
+    head = NULL;          //Setting head/tail/current to 
+    tail = NULL;          //null so that we dont end up 
+    current = NULL;       //with garbage
+    char act = '\0';      //activity 
+    char loc = '\0';      //location 
+    char timeoy = '\0';   //time of year
+    char spectime = '\0'; //specific time(s)
+    char thoughts = '\0'; //thoughts on the activity 
+    char desc = '\0';     //description of activity
 }
 
-void activity::readact()
-{
+void activity::readact()  //Our first function for reading 
+{                         //in activities from the user
     activity newact;
 
-    if(!head)
+    if(!head)             //make sure we have a starting point!
         return;
     while(current != NULL)
     {
--- Prog4/main.cpp
+++ Prog4/main.cpp
@@ -2,7 +2,14 @@
 //2/22/2020
 //CS162
 //Program 4
-//
+//This program will help keep track of user
+//activites. It 'can' be used to store to a 
+//file if needed, but the main points are to
+//use structs/classes/nodes to create the same
+//program as our last one, storing user entered
+//activites with the possibility of recalling 
+//from a file later.
+
 #include "header.h"
 #include "class.h"
 

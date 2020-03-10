013835e924e95fe8dbd636775105da28682aa53b
Justin Greever <jgreever@gmail.com>
2020-03-09 20:34:55 -0700

    Prog5. LLL. skeleton code created

---
+++ Prog5/functions.h
@@ -0,0 +1,17 @@
+/* functions.h 
+ *
+ * Justin Greever
+ * CS162
+ * Program 5
+ * 
+ * This program will use linear linked lists to
+ * store data about 3 items: Name of my favorite thing,
+ * description of the item, and how often I participate
+ * with the item. It will store to a new LLL, display
+ * contents of all items in the list, remove an item
+ * by name, count the number of items in the list and
+ * release all memory (destroy the lll).
+ */
+
+#include "header.h"
+
